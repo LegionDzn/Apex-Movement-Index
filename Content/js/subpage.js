10c7cc71db9376258378c39e4776b1d0467bd7f2
LegionDzn <46273693+LegionDzn@users.noreply.github.com>
2021-11-24 19:51:41 -0800

    v54

--- Content/js/subpage.js
+++
@@ -1,8 +0,0 @@
-var DarkModeDesired = parent.DarkModeDesired;
-if (DarkModeDesired == true) {
-  console.log("turning on dark mode within iframe");
-  document.getElementById("bodytoggle").classList.add('dark');
-} else {
-  console.log("turning off dark mode within iframe");
-  document.getElementById("bodytoggle").classList.remove('dark');
-}
